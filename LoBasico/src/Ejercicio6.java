c16b1c3f0ce9be641a52469fb9db571f33de268f
Dinmy Jose <dcastedo@gamil.com>
2024-10-30 13:41:43 -0400

    remodelanto algo mal heco

--- LoBasico/src/Ejercicio6.java
+++ LoBasico/src/Ejercicio6.java
@@ -1,19 +1,16 @@
 public class Ejercicio6 {
     // ¿Cuáles son los valores de a, b y c después de las siguientes operaciones?
     public static void main(String[] args) {
-        int a1 = 3;
-        int b1 = 4;
+        int a = 3;
+        int b = 4;
         int dos = 2;
-        int c1 = a1 + dos * b1;
-        int c2 = c1 + b1;
-        int b2 = c2 - a1;
-        int a2 = b2 * c2;
-        System.out.println("el valor de a:" + a1);
-        System.out.println("el valor de b1:" + b1);
-        System.out.println("el valor de c1:" + c1);
-        System.out.println("el valor de c2:" + c2);
-        System.out.println("el valor de b2:" + b2);
-        System.out.println("el valor de a2:" + a2);
+        int c = a + dos * b;
+        c = c + b;
+        b = c - a;
+        a = b * c;
+        System.out.println("el valor de a:" + a);
+        System.out.println("el valor de b:" + b);
+        System.out.println("el valor de c:" + c);
     }
 }
 
